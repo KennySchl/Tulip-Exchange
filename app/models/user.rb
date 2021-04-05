46476180733f4ea7f63035af1ef862e16f530006
Kenneth Schlappkohl <kschlappkohl@gmail.com>
2021-04-05 11:08:42 -1000

    User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,39 @@ class User < ApplicationRecord
   validates :email, :session_token, presence: true, uniqueness: true
   validates :password_digest, presence: true
   validates :password, length: { minimum: 6, allow_nil: true }, confirmation: { case_sensitive: true}
+
+  after_initialize :ensure_session_token
+
+  attr_reader :password
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    if user && user.is_password?(password)
+      return user
+    else
+      return nil
+    end
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.base64(64)
+    self.save!
+    self.session_token
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.base64(64)
+  end
+
 end
