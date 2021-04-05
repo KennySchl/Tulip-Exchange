cedcd182dbdf8889899bc4447e33d39d0cbee336
Kenneth Schlappkohl <kschlappkohl@gmail.com>
2021-04-05 12:39:51 -1000

    Complete user and sessions controllers, and added views

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,30 @@
+class Api::SessionsController < ApplicationController
+
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+
+    if @user
+      login(@user)
+      render "api/users/show"
+    else
+      render json: ["Unable to log in with provided credentials."], status: 401
+    end
+  end
+
+
+  def destroy
+    @user = current_user
+
+    if @user
+      logout!
+      render "api/users/show"
+    else
+      # 404 Not Found
+      render json: ["No users logged in."], status: 404
+    end
+  end
+
+end
