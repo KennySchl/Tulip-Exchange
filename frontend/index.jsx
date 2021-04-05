cd0b171e465a8ddf9f74f23023244ca5a6dad7e0
Kenneth Schlappkohl <kschlappkohl@gmail.com>
2021-04-05 13:46:30 -1000

    Implement frontend react

---
+++ frontend/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+document.addEventListener('DOMContentLoaded', ()=> {
+  ReactDOM.render(<h1>REACTING</h1>, document.getElementById('root'))
+})
