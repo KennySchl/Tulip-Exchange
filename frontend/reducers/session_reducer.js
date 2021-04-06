be18cb0f1edb7959ff5c53c154ae21d36876bdc1
Kenneth Schlappkohl <kschlappkohl@gmail.com>
2021-04-05 16:31:37 -1000

    create sessionReducer

---
+++ frontend/reducers/session_reducer.js
@@ -0,0 +1,21 @@
+import {
+  RECEIVE_CURRENT_USER,
+  LOGOUT_CURRENT_USER,
+} from "../actions/session_actions";
+
+const _nullUser = Object.freeze({
+  currentUserId: null,
+});
+
+export const sessionReducer = (state = _nullUser, action) => {
+  Object.freeze(state);
+  const newState = Object.assign({}, state);
+  switch (action.type) {
+    case RECEIVE_CURRENT_USER:
+      return { currentUserId: action.currentUser.id };
+    case LOGOUT_CURRENT_USER:
+      return newState;
+    default:
+      return state;
+  }
+};
