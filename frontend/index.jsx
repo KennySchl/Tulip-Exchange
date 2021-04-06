8a846fdc6a0116fe902de808a6f136d80c178ed1
Kenneth Schlappkohl <kschlappkohl@gmail.com>
2021-04-05 16:41:54 -1000

    create store and import into entry

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Root } from "./components/root";
+import { configureStore } from "./store/store";
 
 document.addEventListener("DOMContentLoaded", () => {
-  ReactDOM.render(<Root />, document.getElementById("root"));
+  ReactDOM.render(<Root store={configureStore()} />, document.getElementById("root"));
 });
