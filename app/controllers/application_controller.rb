381493a1891973f135d56bcb1d48a9d2a0010c1b
Kenneth Schlappkohl <kschlappkohl@gmail.com>
2021-04-05 15:20:20 -1000

    Test Sign in/signout with postman, fix errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < ActionController::Base
 
   def logout!
     current_user.reset_session_token! if logged_in?
-    session_token[:session_token] = nil
+    session[:session_token] = nil
     @current_user = nil
   end
 
