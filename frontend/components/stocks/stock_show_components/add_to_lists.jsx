d64f503ad0c853621d32c23c58718a27270c7099
Kenneth Schlappkohl <kschlappkohl@gmail.com>
2021-05-14 23:50:07 -1000

    Work on add stock to watchlist

--- frontend/components/stocks/stock_show_components/add_to_lists.jsx
+++ frontend/components/stocks/stock_show_components/add_to_lists.jsx
@@ -5,28 +5,28 @@ import { createWatchlistItem } from "../../../util/watchlists_api_util";
 
 const AddToLists = ({ stocks, watchlists, stockSymbol }) => {
   const [stockId, setStockId] = useState(0);
-  const [checkedLists, setCheckedLists] = useState({})
+  const [checkedLists, setCheckedLists] = useState("");
 
-  const lists = {};
   useEffect(() => {
     stocks.forEach((stock) => {
       stockSymbol === stock.symbol ? setStockId(stock.id) : "";
     });
   });
 console.log(watchlists);
-  watchlists.forEach((list, i) => (lists[list.name] = list.name));
+  console.log(stockId);
+  console.log(checkedLists);
 
-  console.log(lists);
-  console.log(checkedLists)
   const watchlistItemCreate = (currentUserId) => {
-    const listItem = { stockId: 5, watchlistId: 1 };
+    const listItem = { stockId: stockId, watchlistId: 1 };
     createWatchlistItem(currentUserId, listItem.watchlistId, listItem);
   };
 
   const handleCheckedLists = (e) => {
-    const {name, id} = e.target
-    setCheckedLists({...checkedLists, [name]: e.target.checked})
-  }
+    const { id, name } = e.target;
+   
+    // setCheckedLists({ ...checkedLists, [id]: e.target.checked });
+    setCheckedLists(parseInt(id));
+  };
 
   return (
     <div className="chart-button-container">
@@ -34,9 +34,15 @@ console.log(watchlists);
         <FontAwesomeIcon icon={faCheck} />
         Add to Lists
       </div>
-      {watchlists.map((list,i) => (
+      {watchlists.map((list, i) => (
         <div key={i}>
-          <input type="checkbox" name={list.name} id={list.id} checked={checkedLists[list.name]} onChange={handleCheckedLists}/>
+          <input
+            type="radio"
+            name="lists"
+            id={list.id}
+            watchlistitems={list.watchlistItems}
+            onChange={handleCheckedLists}
+          />
           {list.name}
         </div>
       ))}
