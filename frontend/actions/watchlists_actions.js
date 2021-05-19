0542c54a03056fa9349a13b52166db9162c75463
Kenneth Schlappkohl <kschlappkohl@gmail.com>
2021-05-18 21:58:00 -1000

    cleanup watchlists reducers and actions

--- frontend/actions/watchlists_actions.js
+++ frontend/actions/watchlists_actions.js
@@ -1,17 +1,10 @@
 import * as WatchlistAPI from "../util/watchlists_api_util";
 
 export const RECEIVE_CURRENT_WATCHLISTS = "RECEIVE_CURRENT_WATCHLISTS";
-export const ADD_WATCHLIST_ITEM = "ADD_WATCHLIST_ITEM";
-export const REMOVE_WATCHLIST_ITEM = "REMOVE_WATCHLIST_ITEM";
 
 const receiveWatchlists = (watchlists) => ({
   type: RECEIVE_CURRENT_WATCHLISTS,
   watchlists,
-});
-
-const removeWatchlistItem = (listItem) => ({
-  type: REMOVE_WATCHLIST_ITEM,
-  listItem,
 });
 
 export const fetchWatchlists = (userId) => (dispatch) =>
--- frontend/reducers/watchlists_reducer.js
+++ frontend/reducers/watchlists_reducer.js
@@ -1,30 +1,13 @@
-import {
-  ADD_WATCHLIST_ITEM,
-  RECEIVE_CURRENT_WATCHLISTS,
-  REMOVE_WATCHLIST_ITEM,
-} from "../actions/watchlists_actions";
+import { RECEIVE_CURRENT_WATCHLISTS } from "../actions/watchlists_actions";
 
 // const _nullWatchlists = Object.freeze({
 //   watchlists: [],
 // });
 
 const watchlistsReducer = (state = [], action) => {
-  let newState = state;
-  // const newState = state;
   switch (action.type) {
     case RECEIVE_CURRENT_WATCHLISTS:
       return action.watchlists;
-    case ADD_WATCHLIST_ITEM:
-      return newState;
-    case REMOVE_WATCHLIST_ITEM:
-      return newState;
-
-    // newState.forEach((watchlist) => {
-    //     watchlist.watchlistItems.filter(
-    //       (listItem) => listItem.id === action.listItem.id
-    //     );
-    //   });
-
     default:
       return state;
   }
